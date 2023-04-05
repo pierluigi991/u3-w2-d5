6580cd28490fbc45dba7cee1a828a326039f3978
pierluigi991 <marzo.pierluigi991@gmail.com>
2023-04-05 17:51:51 +0200

    dynamic

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,12 +1,18 @@
-import { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
-class Films extends Component {
-  render() {
-    return (
-      <div className="col-6 col-md-3 col-lg-2">
-        <img className="img-fluid " src={this.props.src} alt={this.props.alt} />
-      </div>
-    );
-  }
-}
+const Films = (props) => {
+  const navigate = useNavigate();
+  return (
+    <div className="col-6 col-md-3 col-lg-2">
+      <img
+        onClick={() => {
+          navigate(`/movie-details/${props.imdbID}`);
+        }}
+        className="img-fluid "
+        src={props.src}
+        alt={props.alt}
+      />
+    </div>
+  );
+};
 export default Films;
